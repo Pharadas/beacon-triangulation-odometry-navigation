6e5f4637424fcb526b52d6bc3c7fd9d8beac88af
pharadas <davidsudo18@gmail.com>
2023-10-31 14:53:39 -0600

    read and save ptcloud

--- src/ptcloudreader/scripts/ptcloud.py
+++ src/ptcloudreader/scripts/ptcloud.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+from typing import dataclass_transform
 from numpy.core.fromnumeric import shape
 import rospy
 from sensor_msgs.msg import PointCloud2
@@ -8,6 +9,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import struct
+
+import pickle
+from dataclasses import dataclass
+
+@dataclass
+class Point:
+    x: np.float32
+    y: np.float32
+    z: np.float32
+    intensity: np.float32
+    ring: np.uint16
+    time: np.float32
+
+    def __init__(self, x, y, z, intensity, ring, time):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.intensity = intensity
+        self.ring = ring
+        self.time = time
 
 def cartesian_to_spherical(vec):
     x = vec[0]
@@ -20,103 +41,32 @@ def cartesian_to_spherical(vec):
         np.arctan2(np.sqrt(x ** 2 + y ** 2), z)
     ])
 
-last_width = 0
-
-number_of_lines = 300
-list_of_line_scans = [[] for _ in range(number_of_lines)]
+max_scans = 100
+points = [[] for i in range(max_scans)]
 global_counter = 0
-should_start_writing_to_topic = False
 
 def callback(msg):
+    global points
+    global global_counter
+
     print("gaming time")
     # publisher = rospy.Publisher('matrix_image', Float64MultiArray, queue_size=10)
-    global global_counter
-    global list_of_line_scans
-    global number_of_lines
-    global should_start_writing_to_topic
 
-    global last_width
-
-    if global_counter >= number_of_lines:
-        should_start_writing_to_topic = True
-        global_counter = 0
-
-    if should_start_writing_to_topic:
-        matrix_image_msg = Float64MultiArray()
-
-        xs = []
-        ys = []
-        zs = []
-        intensities = []
-        rs = []
-        thetas = []
-        phis = []
-
-        N = 600
-        phis_N = 300
-
-        for line in list_of_line_scans:
-            intensities.extend(line[3])
-            rs.extend(line[4])
-            thetas.extend(line[5])
-            phis.extend(line[6])
-
-        min_thetas = min(thetas)
-        max_thetas = max(thetas)
-        thetas_length = max_thetas - min_thetas
-
-        min_phis = min(phis)
-        max_phis = max(phis)
-        phis_length = max_phis - min_phis
-
-        image = np.zeros((phis_N, N))
-
-        for i in range(len(rs)):
-            if (thetas[i] > min_thetas and thetas[i] < max_thetas) and (rs[i] < 3 and rs[i] > 2):
-                theta_index = int(((thetas[i] - min_thetas) / thetas_length) * N - 1)
-                phi_index = int(((phis[i] - min_phis) / phis_length) * phis_N - 1)
-
-                image[phi_index][theta_index] = rs[i]
-
-        final_image = []
-
-        # shouldnt do this, should make it flat from the beginning
-        for i in image:
-            final_image.extend(i)
-
-        matrix_image_msg.data = final_image
-        matrix_image_msg.layout.data_offset = 0
-        publisher.publish(matrix_image_msg)
-
-    xs = []
-    ys = []
-    zs = []
-    intensities = []
-    rs = []
-    thetas = []
-    phis = []
+    new_points = []
+    retroreflector_points = []
 
     for i in range(0, len(msg.data), 16):
-        x_float = struct.unpack('f', msg.data[i : i+4])
-        xs.append(x_float)
+        x = struct.unpack('f', msg.data[i : i+4])
+        y = struct.unpack('f', msg.data[i+4 : i+8])
+        z = struct.unpack('f', msg.data[i+8 : i+12])
+        intensity = struct.unpack('f', msg.data[i+12 : i+16])
+        ring = struct.unpack('H', msg.data[i+16 : i+18])
+        time = struct.unpack('f', msg.data[i+18 : i+22])
 
-        y_float = struct.unpack('f', msg.data[i+4 : i+8])
-        ys.append(y_float)
+        new_points.append(Point(x, y, z, intensity, ring, time))
 
-        z_float = struct.unpack('f', msg.data[i+8 : i+12])
-        zs.append(z_float)
-
-        intensity_float = struct.unpack('f', msg.data[i+12 : i+16])
-        intensities.append(intensity_float[0])
-
-        spherical_coords = cartesian_to_spherical(np.array([x_float, y_float, z_float]))
-        rs.append(spherical_coords[0])
-        thetas.append(spherical_coords[1])
-        phis.append(spherical_coords[2])
-
-    print(global_counter)
-    list_of_line_scans[global_counter] = [xs, ys, zs, intensities, rs, thetas, phis]
-    global_counter += 1
+    points[global_counter] = new_points
+    global_counter %= max_scans
 
 def listener():
     topic = rospy.get_param('~topic', '/yrl_pub/yrl_cloud')
@@ -126,6 +76,6 @@ def listener():
 if __name__ == '__main__':
     rospy.init_node('pylistener', anonymous = True)
     listener()
-    # outfile = TemporaryFile()
-    # np.save(outfile, list_of_line_scans)
 
+    with open('velodyne_point_cloud_1.pkl', 'wb') as file:
+        pickle.dump(points, file)
